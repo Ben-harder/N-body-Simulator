004403e7af950abb7bf39851edbb0e083ef3daea
Ben Harder <31527486+Ben-harder@users.noreply.github.com>
2018-05-04 11:50:41 -0700

    Added resizing canvas but nothing drawing

--- JavaScript/n-body.js
+++ JavaScript/n-body.js
@@ -2,32 +2,39 @@
  * This file will contain the main code for drawing and calculating positions.
  */
 
-var canvas = document.getElementById("canvas");
-var canvasWidth = canvas.width;
-var canvasHeight = canvas.height;
 var refresh = setInterval(frame, 20);
 
 var particles = [];
 
-const ctx = canvas.getContext('2d');
 
-var pp = new Particle(10, 20, 20);
+var pp = new Particle(100, 20, 20);
 
 particles.push(pp);
-function frame() 
+function frame()
 {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    var canvas = document.getElementById("canvas");
 
-    ctx.fillStyle = "#999999";
-    
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "#f00";
+
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    console.log(ctx.canvas.width);
+    console.log(ctx.canvas.height);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(pp.x, pp.y, pp.mass, pp.mass);
+
+    ctx.fillRect(1, 1, 10, 10);
     // Update the position and move all the particles.
-    for(var i = 0; i < particles.length; i++)
+    for (var i = 0; i < particles.length; i++)
     {
         var currParticle = particles[i];
-        
+
         // Perform particle math here:
         // Need to calculate the force between this particle and all other particles.
-
+        //console.log(currParticle);
 
         currParticle.update(); // Update the position of the particle
 
@@ -50,17 +57,17 @@ function calculateForce(particle)
     for (var i = 0; i < particles.length; i++)
     {
         var currParticle = particles[i];
-        
+
         // Account for the particle not being the pass particle
         if (!currParticle.equal(particle))
         {
             var massProduct = currParticle.mass * particle.mass;
-            
+
             var netParticleMag = Math.sqrt(Math.pow(currParticle.x - particle.x, 2) + Math.pow(currParticle.y - particle.y, 2));
             // 
             var netMagCubed = Math.pow(currParticleMag - particleMag, 3);
 
-            netForceX = massProduct * (currParticle.x - particle.x) / netMagCubed
+            // netForceX = massProduct * (currParticle.x - particle.x) / netMagCubed
         }
     }
 }
