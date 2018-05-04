dc6501a2beda1d7349fd911e6dc05955314f3d11
ben <ben.harder2@gmail.com>
2018-05-04 14:03:51 -0700

    Changed to use pair structure

--- JavaScript/particle.js
+++ JavaScript/particle.js
@@ -7,44 +7,35 @@ class Particle
     constructor(mass, x, y)
     {
         this.mass = mass;
-        this.xV = 0;
-        this.yV = 0;
-        this.xA = 0;
-        this.yA = 0;
-
-        this.x = x;
-        this.y = y;
+        this.V = new Pair(0, 0);
+        this.A = new Pair(0, 0);
+        this.pos = new Pair(x, y);
     }
 
     setVelocity(xV, yV)
     {
-        this.xV = xV;
-        this.yV = yV;
+        this.V.set(xV, yV);
     }
 
     setAcceleration(xA, yA)
     {
-        this.xA = xA;
-        this.yA = yA;
+        this.A.set(xA, yA);
     }
 
     update()
     {
-        this.x += this.xV;
-        this.y += this.yV;
+        this.pos.first += this.V.first;
+        this.pos.second += this.V.second;
 
-        this.xV += this.xA;
-        this.yV += this.yA;
+        this.V.first += this.A.first;
+        this.V.second += this.A.second;
     }
 
     equal(other)
     {
-        return (this.x == other.x &&
-            this.y == other.y && 
-            this.xV == other.xV && 
-            this.yV == other.yV && 
-            this.xA == other.xA && 
-            this.yA == other.yA &&  
+        return (this.V.equals(other.V) &&
+            this.A.equals(other.A) &&
+            this.pos.equals(other.pos) &&
             this.mass == other.mass);
     }
     
