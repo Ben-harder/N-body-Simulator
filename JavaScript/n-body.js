193d173ead6bf46943ace0c96f486374d355be23
ben <ben.harder2@gmail.com>
2018-05-04 16:48:41 -0700

    Clicking and dragging done as well as deleting particles

--- JavaScript/n-body.js
+++ JavaScript/n-body.js
@@ -16,43 +16,55 @@ particles.push(jj);
 particles.push(gg);
 
 
-function frame() {
-
+function frame()
+{
     draw(ctx);
 }
 
 /*
  * This function will draw all the particles in their current state.
  */
-function draw(ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#FF0000";
+function draw(ctx)
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    ctx.fillStyle = "#FFFFFF";
+    
     // Update the position and move all the particles.
-    for (var i = 0; i < particles.length; i++) 
+    for (var i = 0; i < particles.length; i++)
     {
         var currParticle = particles[i];
-
-        // Perform particle math here:
-        // Need to calculate the force between this particle and all other particles.
-        //console.log(currParticle);
-        var netForce = calculateForce(currParticle);
-
-        // Use net force to find acceleration.
-        currParticle.A.set(netForce.first/currParticle.mass, netForce.second/currParticle.mass);
-
-        currParticle.update(); // Update the position of the particle
-
-        // Area of the circle will be proportional to it's mass.
-        // A = pi*r^2 => r = sqrt(A/pi) where A = mass
-        var radius = Math.sqrt(currParticle.mass/Math.PI) * 2;
-
-        // Draw the particle
-        ctx.beginPath();
-
-        ctx.ellipse(currParticle.pos.first, currParticle.pos.second, radius, radius, 0, 0, 2 * Math.PI);
-        
-        ctx.fill();
+        // Check to see if the particle left the canvas
+        if (currParticle.pos.first > canvas.width || currParticle.pos.second > canvas.height || currParticle.pos.first < 0 || currParticle.pos.second < 0)
+        {
+            delete currParticle;
+            particles.splice(i, 1);
         }
+        else
+        {
+            // Calculate the netforce vector between this particle and all others.
+            var netForce = calculateForce(currParticle);
+
+            // Use net force to find acceleration.
+            currParticle.A.set(netForce.first / currParticle.mass, netForce.second / currParticle.mass);
+
+            currParticle.update(); // Update the position of the particle
+
+            // Area of the circle will be proportional to it's mass.
+            // A = pi*r^2 => r = sqrt(A/pi) where A = mass
+            var radius = Math.sqrt(currParticle.mass / Math.PI) * 2;
+
+            // Draw the particle
+            ctx.beginPath();
+
+            ctx.ellipse(currParticle.pos.first, currParticle.pos.second, radius, radius, 0, 0, 2 * Math.PI);
+
+            ctx.fill();
+        }
+
+
+        // Display Particle count
+        document.getElementById("particleCount").innerHTML = "Particle count: " + particles.length;
+    }
 }
 
 /*
@@ -61,15 +73,18 @@ function draw(ctx) {
  * 
  * Formula found here: https://en.wikipedia.org/wiki/N-body_problem
  */
-function calculateForce(particle) {
+function calculateForce(particle)
+{
     var netForceX = 0;
     var netForceY = 0;
 
-    for (var i = 0; i < particles.length; i++) {
+    for (var i = 0; i < particles.length; i++)
+    {
         var currParticle = particles[i];
 
         // Account for the particle not being the passed particle
-        if (!currParticle.equals(particle)) {
+        if (!currParticle.equals(particle))
+        {
             var massProduct = currParticle.mass * particle.mass;
             var netParticleMag = Math.sqrt(Math.pow(currParticle.pos.first - particle.pos.first, 2) + Math.pow(currParticle.pos.second - particle.pos.second, 2));
             var netMagCubed = Math.pow(netParticleMag, 3);
@@ -81,3 +96,33 @@ function calculateForce(particle) {
 
     return new Pair(netForceX, netForceY);
 }
+
+/*
+ * Click event to spawn a particle.
+ */
+var startPos;
+
+canvas.addEventListener("mousedown", function ()
+{
+    //console.log("Mouse down at " + (event.pageX - canvas.offsetLeft) + ", " + (event.pageY - canvas.offsetTop));
+
+    startPos = new Pair(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+}, false);
+
+canvas.addEventListener("mouseup", function ()
+{
+    //console.log("Mouse up at " + (event.pageX - canvas.offsetLeft) + ", " + (event.pageY - canvas.offsetTop));
+
+    // Mass equals the vector magnitude between mouse down and mouse up
+    var endPos = new Pair(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    var dx = Math.abs(endPos.first - startPos.first);
+    var dy = Math.abs(endPos.second - startPos.second);
+    var mass = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+    if (mass <= 0)
+        mass = 1;
+
+    var particle = new Particle(mass, startPos.first, startPos.second);
+    particles.push(particle);
+
+}, false);
